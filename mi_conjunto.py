9cdd8489ef4823e238331d821cca77f937aa1626
Janna Ramirez Granados <a.22308051281287@cbtis128.edu.mx>
2024-09-11 07:37:58 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print(" -- EJEMPLO DE CONJUNTOS SET -- ")
+tipodeportes =  {"futbol", "tenis", "basquetball"}
+print(tipodeportes)
+print(" -- LISTA ELEMENTOS DEL CONJUNTO TIPODEPORTES LOOP FOR -- ")
+for ramirez in tipodeportes:
+    print(ramirez)
